921930d25e8d236b0199856e04d644b6f679a8e3
Kate <katerebekah@gmail.com>
2016-05-14 09:28:10 -0500

    Updating Sherlock

--- spec/SherlockHolmes.js
+++ spec/SherlockHolmes.js
@@ -17,12 +17,26 @@ describe("A Study In Scarlet", function() {
   });
 });
 
-/*describe("The Sign of the Four", function() {
+describe("The Sign of the Four", function() {
   it("", function() {
-		var TheSignOfTheFour;
+		var Thaddeus = ['pearls', 'gold', 'diamonds', 'silver', 'emeralds'];
+		var Mary = [12, 53, 21, 67, 34];
+		var TreasureCount = function(array, array2){
+			var returnObject = [];
+			/*for (i= 0; i < array.length; i++){
+				returnObject.push([array[i]]);
+			}
+			for (var i=array2.length-1; i > -1; i--){
+				console.log(returnObject[i]);
+//				returnObject[i].push(array2[i]);
+			}*/
+			return returnObject;
+		}
+		expect(TreasureCount(Thaddeus, Mary)).toEqual([ [ 'pearls', 34 ], [ 'gold', 67 ], [ 'diamonds', 21 ], [ 'silver', 53 ], [ 'emeralds', 12 ] ])
 	});
 });
 
+/*
 describe("The Hound of Baskerville", function() {
   it("", function() {
 		var TheHoundOfBaskerville;
