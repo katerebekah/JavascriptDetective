2a96e494cc5b12ca47cdf86c5e2c2d5591c9e1d0
Kate <katerebekah@gmail.com>
2016-05-15 13:49:09 -0500

    looking to split detectives into topics

--- spec/SherlockHolmes.js
+++ spec/SherlockHolmes.js
@@ -1,5 +1,10 @@
+// <IGNORE ME>
+var i;
+var heirs;
+// </IGNORE ME>
+
 describe("A Study In Scarlet", function() {
-  it("should return a+b", function() {
+  it("should have access to PoisonedPill.a", function() {
 		var PoisonedPill = function (){
 			var a = 7;
 			var c = 4;
@@ -18,36 +23,63 @@ describe("A Study In Scarlet", function() {
 });
 
 describe("The Sign of the Four", function() {
-  it("", function() {
+  it("should reverse Mary's fortunes.", function() {
+
+  	//each of the values of the Thaddeus array 
+  	//should match up to the reverse value in Mary's array.
+		
 		var Thaddeus = ['pearls', 'gold', 'diamonds', 'silver', 'emeralds'];
 		var Mary = [12, 53, 21, 67, 34];
+		
 		var TreasureCount = function(array, array2){
 			var returnObject = [];
-			/*for (i= 0; i < array.length; i++){
-				returnObject.push([array[i]]);
+		
+			for (i= 0; i < array.length; i++){
+				returnObject.push([array[i], 0]);
 			}
-			for (var i=array2.length-1; i > -1; i--){
-				console.log(returnObject[i]);
-//				returnObject[i].push(array2[i]);
-			}*/
+		
+			for (i=array2.length-1; i > -1; i--){
+				returnObject[i][1] = array2.reverse()[i];
+			}
+		
 			return returnObject;
 		}
 		expect(TreasureCount(Thaddeus, Mary)).toEqual([ [ 'pearls', 34 ], [ 'gold', 67 ], [ 'diamonds', 21 ], [ 'silver', 53 ], [ 'emeralds', 12 ] ])
 	});
 });
 
-/*
-describe("The Hound of Baskerville", function() {
-  it("", function() {
-		var TheHoundOfBaskerville;
+describe("The Hound of the Baskervilles", function() {
+  it("should only reveal the correct values outside the loop.", function() {
+		// value[0] is name of potential heir, value[1] is whether they are alive  
+		var TheHoundOfTheBaskervilles = [["Charles", false], ["Henry", true], ["Rodger", true]];
+
+		function whoCanInheritTheFortune (array){
+
+			var findTheHeirs = function(){
+				for (i = 0; i < array.length; i++){
+					var heirs = [];
+					if(!array[i][1]){
+						heirs.push(array[i][0]);
+					}
+				}	
+				return heirs
+			}
+
+			findTheHeirs();
+
+			return {heirs: heirs};
+		}
+
+		var heir = whoCanInheritTheFortune(TheHoundOfTheBaskervilles).heirs;
+
+		expect(i).not.toBeDefined();
+		expect(heir).toBeDefined();
 	});
 });
 
-describe("The Valley of Fear", function() {
+
+/*describe("The Valley of Fear", function() {
   it("", function() {
 		var TheValleyOfFear;
   });
-});
-
-
-*/
+});*/
